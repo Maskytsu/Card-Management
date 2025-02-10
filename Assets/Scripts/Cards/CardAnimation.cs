0b1158164a405da897515eb08b360522f45df622
Masky <biloxwolf@gmail.com>
2025-02-10 18:36:57 +0100

    updated method description

--- Assets/Scripts/Cards/CardAnimation.cs
+++ Assets/Scripts/Cards/CardAnimation.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public abstract class CardAnimation : MonoBehaviour
 {
     /// <summary>
-    /// Animation should be created as part of given Sequence <paramref name="animationSeq"/>.
+    /// Animation should be created as part of given <paramref name="animationSeq"/> on the <paramref name="transformToAnimate"/>.
     /// </summary>
     public abstract void CardPlayAnimation(Sequence animationSeq, Transform transformToAnimate);
 }
